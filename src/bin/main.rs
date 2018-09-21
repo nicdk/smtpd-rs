6da399f47c4d25c98533f5b11aa5fea0e86016db
Kei NIKADO <nikado.kei@gmail.com>
2018-09-21 14:25:05 +0900

    update: find Maildir/new directory files.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,6 +2,8 @@ extern crate hostname;
 extern crate bufstream;
 
 use std::net::TcpListener;
+use std::fs;
+use std::path::Path;
 use std::io;
 use std::thread::spawn;
 use bufstream::BufStream;
@@ -9,11 +11,32 @@ use bufstream::BufStream;
 mod receiver;
 
 fn main() {
+    println!("hello, here is main.");
     smtpd("127.0.0.1:8025").expect("error: ");
 }
 
 fn smtpd(addr: &str) -> io::Result<()> {
     receiver::main();
+
+    let maildir = fs::read_dir("tests/Maildir")?;
+    println!("{:?}", maildir);
+    
+    for _entry_result in maildir {
+        let _entry = _entry_result?;
+        if _entry.path().is_dir() && _entry.file_name() == "new" {
+            for _entry_new_result in fs::read_dir(_entry.path())? {
+                let _entry_new = _entry_new_result?;
+                println!("{:?}", _entry_new.path());
+                if !_entry_new.path().is_file() {
+                    continue;
+                }
+                let src = _entry_new.path();
+                let dst = Path::new("tests/Maildir/tmp/").join(_entry_new.file_name());
+                println!("mv: {:?} -> {:?}", src, dst);
+                fs::rename(src, dst)?;
+            }
+        }
+    }
     
     let listener = TcpListener::bind(addr)?;
     println!("smtpd-rs: listening on {}", addr);
