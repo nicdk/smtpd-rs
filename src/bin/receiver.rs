f0ecd62df644a3325986a8ce51e2347711ce9778
Kei NIKADO <nikado.kei@gmail.com>
2018-09-26 21:33:36 +0900

    update: mail data build.

--- src/bin/receiver.rs
+++ src/bin/receiver.rs
@@ -16,6 +16,23 @@ enum SmtpState {
 }
 
 #[derive(Debug, PartialEq)]
+struct ReceiveMail {
+    pub mail_from: String,
+    pub rcpt_to: String,
+    pub data: String
+}
+
+impl ReceiveMail {
+    pub fn init() -> ReceiveMail {
+        ReceiveMail {
+            mail_from: String::new(),
+            rcpt_to: String::new(),
+            data: String::new()
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 struct SmtpReply {
     pub code: u16,
     pub message: String
@@ -29,76 +46,243 @@ pub fn handler(stream: &mut BufStream<TcpStream>) -> io::Result<()> {
     stream.write(b"220 hostname ESMTP smtpd-rs\n")?;
     stream.flush()?;
     let mut state = SmtpState::INIT;
+    let mut _mail = ReceiveMail::init();
 
     loop {
-        let mut line = String::new();
-        if let Err(err) = stream.read_line(&mut line) {
-            print_error(&err);
-            panic!("error: {}", err);
-        }
+        state = match &state {
+            SmtpState::INIT => {
+                let mut line = String::new();
+                if let Err(err) = stream.read_line(&mut line) {
+                    print_error(&err);
+                    panic!("error: {}", err);
+                }
+                println!("line {:?}", line.trim());
 
-        let _cmd;
-        match line.trim().split_whitespace().nth(0) {
-            Some(s) => _cmd = s,
-            None => {
-                stream.write(b"555 5.5.2 Syntax error. - smtpd-rs")?;
-                stream.flush()?;
-                continue;
-            }
-        };
-        println!("command {}", _cmd);
+                let _cmd;
+                match line.trim().split_whitespace().nth(0) {
+                    Some(s) => _cmd = s,
+                    None => {
+                        stream.write(format!("555 5.5.2 Syntax error. - smtpd-rs\n").as_bytes())?;
+                        stream.flush()?;
+                        continue;
+                    }
+                };
+                println!("command {}", _cmd);
 
-        match dispatch(&line) {
-            Some(_reply) => {
-                let _code = &_reply.code;
-                let _message: &str = &_reply.message;
-                stream.write(&_message.as_bytes())?;
-                stream.flush()?;
+                match dispatch(&line.trim().to_string()) {
+                    Some(_reply) => {
+                        let _code = &_reply.code;
+                        let _message: &str = &_reply.message;
+                        stream.write(&_message.as_bytes())?;
+                        stream.flush()?;
 
-                state = match &state {
-                    SmtpState::INIT => {
-                        match &_cmd {
-                            &"HELO" => SmtpState::READY,
-                            &"EHLO" => SmtpState::READY,
-                            &"RSET" => SmtpState::READY,
-                            _ => SmtpState::INIT,
-                        }
                     }
-                    SmtpState::READY => {
-                        match &_cmd {
-                            &"MAIL" => SmtpState::MAIL,
-                            &"RSET" => SmtpState::READY,
-                            _ => SmtpState::READY,
-                        }
-                    }
-                    SmtpState::MAIL => {
-                        match &_cmd {
-                            &"RCPT" => SmtpState::RCPT,
-                            &"RSET" => SmtpState::READY,
-                            _ => SmtpState::MAIL,
-                        }
-                    }
-                    SmtpState::RCPT => {
-                        match &_cmd {
-                            &"DATA" => SmtpState::DATA,
-                            &"RSET" => SmtpState::READY,
-                            _ => SmtpState::RCPT,
-                        }
-                    }
-                    SmtpState::DATA => {
-                        match &_cmd {
-                            &"DATA" => SmtpState::READY,
-                            &"RSET" => SmtpState::READY,
-                            _ => SmtpState::DATA,
-                        }
+                    None => {
+                        /* abort. */
+                        stream.write(b"disconnect.")?;
+                        stream.flush()?;
+                        panic!();
                     }
                 }
+
+                match &_cmd {
+                    &"HELO" => SmtpState::READY,
+                    &"EHLO" => SmtpState::READY,
+                    &"RSET" => SmtpState::READY,
+                    &"QUIT" => {
+                        /* abort. */
+                        stream.write(b"disconnect.\n")?;
+                        stream.flush()?;
+                        panic!();
+                    },
+                    _ => SmtpState::INIT,
+                }
             }
-            None => {
-                /* abort. */
-                stream.write(b"disconnect.")?;
-                stream.flush()?;
-                panic!();
+            SmtpState::READY => {
+                let mut line = String::new();
+                if let Err(err) = stream.read_line(&mut line) {
+                    print_error(&err);
+                    panic!("error: {}", err);
+                }
+                println!("line {:?}", line.trim());
+
+                let _cmd;
+                match line.trim().split_whitespace().nth(0) {
+                    Some(s) => _cmd = s,
+                    None => {
+                        stream.write(b"555 5.5.2 Syntax error. - smtpd-rs")?;
+                        stream.flush()?;
+                        continue;
+                    }
+                };
+                println!("command {}", _cmd);
+
+                match dispatch(&line.trim().to_string()) {
+                    Some(_reply) => {
+                        let _code = &_reply.code;
+                        let _message: &str = &_reply.message;
+                        stream.write(&_message.as_bytes())?;
+                        stream.flush()?;
+
+                        _mail.mail_from = match line.trim().split_whitespace().nth(1) {
+                            Some(s) => String::from(s),
+                            None => String::new(),
+                        };
+                    }
+                    None => {
+                        /* abort. */
+                        stream.write(b"disconnect.")?;
+                        stream.flush()?;
+                        panic!();
+                    }
+                }
+
+                match &_cmd {
+                    &"MAIL" => SmtpState::MAIL,
+                    &"RSET" => SmtpState::READY,
+                    &"QUIT" => {
+                        /* abort. */
+                        stream.write(b"disconnect.\n")?;
+                        stream.flush()?;
+                        panic!();
+                    },
+                    _ => SmtpState::READY,
+                }
+            }
+            SmtpState::MAIL => {
+                let mut line = String::new();
+                if let Err(err) = stream.read_line(&mut line) {
+                    print_error(&err);
+                    panic!("error: {}", err);
+                }
+                println!("line {:?}", line.trim());
+
+                let _cmd;
+                match line.trim().split_whitespace().nth(0) {
+                    Some(s) => _cmd = s,
+                    None => {
+                        stream.write(b"555 5.5.2 Syntax error. - smtpd-rs")?;
+                        stream.flush()?;
+                        continue;
+                    }
+                };
+                println!("command {}", _cmd);
+
+                match dispatch(&line.trim().to_string()) {
+                    Some(_reply) => {
+                        let _code = &_reply.code;
+                        let _message: &str = &_reply.message;
+                        stream.write(&_message.as_bytes())?;
+                        stream.flush()?;
+
+                        _mail.rcpt_to = match line.trim().split_whitespace().nth(1) {
+                            Some(s) => String::from(s),
+                            None => String::new(),
+                        };
+                    }
+                    None => {
+                        /* abort. */
+                        stream.write(b"disconnect.")?;
+                        stream.flush()?;
+                        panic!();
+                    }
+                }
+
+                match &_cmd {
+                    &"RCPT" => SmtpState::RCPT,
+                    &"RSET" => SmtpState::READY,
+                    &"QUIT" => {
+                        /* abort. */
+                        stream.write(b"disconnect.\n")?;
+                        stream.flush()?;
+                        panic!();
+                    },
+                    _ => SmtpState::MAIL,
+                }
+            }
+            SmtpState::RCPT => {
+                let mut line = String::new();
+                if let Err(err) = stream.read_line(&mut line) {
+                    print_error(&err);
+                    panic!("error: {}", err);
+                }
+                println!("line {:?}", line.trim());
+
+                let _cmd;
+                match line.trim().split_whitespace().nth(0) {
+                    Some(s) => _cmd = s,
+                    None => {
+                        stream.write(b"555 5.5.2 Syntax error. - smtpd-rs")?;
+                        stream.flush()?;
+                        continue;
+                    }
+                };
+                println!("command {}", _cmd);
+
+                match dispatch(&line.trim().to_string()) {
+                    Some(_reply) => {
+                        let _code = &_reply.code;
+                        let _message: &str = &_reply.message;
+                        stream.write(&_message.as_bytes())?;
+                        stream.flush()?;
+
+                    }
+                    None => {
+                        /* abort. */
+                        stream.write(b"disconnect.\n")?;
+                        stream.flush()?;
+                        panic!();
+                    }
+                }
+
+                match &_cmd {
+                    &"DATA" => SmtpState::DATA,
+                    &"RSET" => SmtpState::READY,
+                    &"QUIT" => {
+                        /* abort. */
+                        stream.write(b"disconnect.\n")?;
+                        stream.flush()?;
+                        panic!();
+                    },
+                    _ => SmtpState::RCPT,
+                }
+            }
+            SmtpState::DATA => {
+                let mut _data = Vec::<String>::new();
+                loop {
+                    let mut _l = String::new();
+                    if let Err(e) = stream.read_line(&mut _l) {
+                        print_error(&e);
+                        continue;
+                    }
+                    println!("line {:?}", _l.trim());
+
+                    if _l.trim().to_string() == "." { break; }
+                    if _l.trim().to_string() == ".."  { _data.push(".".to_string()); continue; }
+
+                    _data.push(_l);
+                }
+                _mail.data = _data.concat();
+
+                if _mail.mail_from.is_empty() {
+                    println!("mail from is empty.");
+                    stream.write(format!("451 Reequested action aborted. FROM address is empty.\n").as_bytes())?;
+                    stream.flush()?;
+                } else if _mail.rcpt_to.is_empty() {
+                    println!("rcpt to is empty.");
+                    stream.write(format!("451 Reequested action aborted.RCPT address is empty.\n").as_bytes())?;
+                    stream.flush()?;
+                } else if _mail.data.is_empty() {
+                    println!("data is empty.");
+                    stream.write(format!("451 Reequested action aborted. DATA context is empty.\n").as_bytes())?;
+                    stream.flush()?;
+                } else {
+                    println!("write mail. {:?}", _mail);
+                    stream.write(format!("250 OK\n").as_bytes())?;
+                    stream.flush()?;
+                }
+
+                SmtpState::READY
             }
         }
     }
@@ -118,19 +302,19 @@ fn dispatch(command: &String) -> Option<SmtpReply> {
                 None => {
                     reply = SmtpReply{
                         code: 555,
-                        message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                        message: format!("5.5.2 Syntax error. - smtpd-rs\n")
                     }
                 }
                 Some(param1) => {
                     if command_split.next() != None {
                         reply = SmtpReply{
                             code: 555,
-                            message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                            message: format!("5.5.2 Syntax error. - smtpd-rs\n")
                         };
                     } else {
                         reply = SmtpReply{
                             code: 250,
-                            message: format!(r"ok at your service {:?}", param1)
+                            message: format!("ok at your service {:?}\n", param1)
                         };
                     }
                 }
@@ -145,19 +329,19 @@ fn dispatch(command: &String) -> Option<SmtpReply> {
                 None => {
                     reply = SmtpReply{
                         code: 555,
-                        message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                        message: format!("5.5.2 Syntax error. - smtpd-rs\n")
                     }
                 }
                 Some(param1) => {
                     if command_split.next() != None {
                         reply = SmtpReply{
                             code: 555,
-                            message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                            message: format!("5.5.2 Syntax error. - smtpd-rs\n")
                         };
                     } else {
                         reply = SmtpReply{
                             code: 250,
-                            message: format!(r"ok at your service {:?}", param1)
+                            message: format!("ok at your service {:?}\n", param1)
                         };
                     }
                 }
@@ -169,7 +353,7 @@ fn dispatch(command: &String) -> Option<SmtpReply> {
             println!("{:?}", from_address);
             let reply = SmtpReply{
                 code: 250,
-                message: format!(r"2.1.0 OK - smtpd-rs")
+                message: format!("2.1.0 OK - smtpd-rs\n")
             };
             Some(reply)
         }
@@ -178,49 +362,49 @@ fn dispatch(command: &String) -> Option<SmtpReply> {
             println!("{:?}", rcpt_address);
             let reply = SmtpReply{
                 code: 250,
-                message: format!(r"2.1.0 OK - smtpd-rs")
+                message: format!("2.1.0 OK - smtpd-rs\n")
             };
             Some(reply)
         }
         Some("DATA") => {
             let reply = SmtpReply{
-                code: 250,
-                message: format!(r"2.1.0 OK - smtpd-rs")
+                code: 354,
+                message: format!("Start mail input; end with <CRLF>.<CRLF>\n")
             };
             Some(reply)
         }
         Some("RSET") => {
             let reply = SmtpReply{
                 code: 250,
-                message: format!(r"2.1.0 OK - smtpd-rs")
+                message: format!("2.1.0 OK - smtpd-rs\n")
             };
             Some(reply)
         }
         Some("NOOP") => {
             let reply = SmtpReply{
                 code: 250,
-                message: format!(r"2.1.0 OK - smtpd-rs")
+                message: format!("2.1.0 OK - smtpd-rs\n")
             };
             Some(reply)
         }
         Some("QUIT") => {
             let reply = SmtpReply{
                 code: 221,
-                message: format!(r"2.0.0 closing connection smtpd-rs")
+                message: format!("Closing connection. Good bye.\n")
             };
             Some(reply)
         }
         Some("VRFY") => {
             let reply = SmtpReply{
                 code: 250,
-                message: format!(r"2.1.0 OK - smtpd-rs")
+                message: format!("2.1.0 OK - smtpd-rs\n")
             };
             Some(reply)
         }
         _ => {
             let reply = SmtpReply{
                 code: 555,
-                message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                message: format!(r"5.5.2 Syntax error. - smtpd-rs\n")
             };
             Some(reply)
         }
@@ -241,17 +425,17 @@ fn test_handler() {
 
 #[test]
 fn test_dispatch() {
-    assert_eq!(dispatch(&"EHLO localhost".to_string()), Some(SmtpReply{code: 250, message: "ok at your service \"localhost\"".parse().unwrap()}));
-    assert_eq!(dispatch(&"HELO localhost".to_string()), Some(SmtpReply{code: 250, message: "ok at your service \"localhost\"".parse().unwrap()}));
+    assert_eq!(dispatch(&"EHLO localhost".to_string()), Some(SmtpReply{ code: 250, message: "ok at your service \"localhost\"\n".parse().unwrap() }));
+    assert_eq!(dispatch(&"HELO localhost".to_string()), Some(SmtpReply{ code: 250, message: "ok at your service \"localhost\"\n".parse().unwrap() }));
 
-    assert_eq!(dispatch(&"MAIL FROM:postmaster@example.com".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
-    assert_eq!(dispatch(&"RCPT TO:postmaster@example.com".to_string()),   Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
-    assert_eq!(dispatch(&"DATA".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"MAIL FROM:postmaster@example.com".to_string()), Some(SmtpReply{ code: 250, message: "2.1.0 OK - smtpd-rs\n".parse().unwrap() }));
+    assert_eq!(dispatch(&"RCPT TO:postmaster@example.com".to_string()),   Some(SmtpReply{ code: 250, message: "2.1.0 OK - smtpd-rs\n".parse().unwrap() }));
+    assert_eq!(dispatch(&"DATA".to_string()), Some(SmtpReply { code: 354, message: "Start mail input; end with <CRLF>.<CRLF>\n".parse().unwrap() }));
 
-    assert_eq!(dispatch(&"RSET".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
-    assert_eq!(dispatch(&"NOOP".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
-    assert_eq!(dispatch(&"QUIT".to_string()), Some(SmtpReply{code: 221, message: r"2.0.0 closing connection smtpd-rs".to_string()}));
-    assert_eq!(dispatch(&"VRFY".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"RSET".to_string()), Some(SmtpReply{code: 250, message: "2.1.0 OK - smtpd-rs\n".parse().unwrap() }));
+    assert_eq!(dispatch(&"NOOP".to_string()), Some(SmtpReply{code: 250, message: "2.1.0 OK - smtpd-rs\n".parse().unwrap() }));
+    assert_eq!(dispatch(&"QUIT".to_string()), Some(SmtpReply{code: 221, message: "Closing connection. Good bye.\n".to_string() }));
+    assert_eq!(dispatch(&"VRFY".to_string()), Some(SmtpReply{code: 250, message: "2.1.0 OK - smtpd-rs\n".parse().unwrap() }));
 
-    assert_eq!(dispatch(&"blahblahblah".to_string()), Some(SmtpReply{code: 555, message: r"5.5.2 Syntax error. - smtpd-rs".to_string()}));
+    assert_eq!(dispatch(&"blahblahblah".to_string()), Some(SmtpReply{code: 555, message: r"5.5.2 Syntax error. - smtpd-rs\n".to_string() }));
 }
