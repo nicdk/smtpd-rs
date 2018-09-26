fb7dce29e05e49a7c28ecbc1a4ab547996fedd9f
Kei NIKADO <nikado.kei@gmail.com>
2018-09-26 17:51:28 +0900

    update: add smtp status.

--- src/bin/receiver.rs
+++ src/bin/receiver.rs
@@ -7,6 +7,14 @@ use std::io;
 use std::io::{Write,BufRead,stderr};
 use bufstream::BufStream;
 
+enum SmtpState {
+    INIT,
+    READY,
+    MAIL,
+    RCPT,
+    DATA,
+}
+
 #[derive(Debug, PartialEq)]
 struct SmtpReply {
     pub code: u16,
@@ -20,6 +28,7 @@ pub fn main() {
 pub fn handler(stream: &mut BufStream<TcpStream>) -> io::Result<()> {
     stream.write(b"220 hostname ESMTP smtpd-rs\n")?;
     stream.flush()?;
+    let mut state = SmtpState::INIT;
 
     loop {
         let mut line = String::new();
@@ -28,14 +37,65 @@ pub fn handler(stream: &mut BufStream<TcpStream>) -> io::Result<()> {
             panic!("error: {}", err);
         }
 
-        match dispatch(line) {
+        let _cmd;
+        match line.trim().split_whitespace().nth(0) {
+            Some(s) => _cmd = s,
+            None => {
+                stream.write(b"555 5.5.2 Syntax error. - smtpd-rs")?;
+                stream.flush()?;
+                continue;
+            }
+        };
+        println!("command {}", _cmd);
+
+        match dispatch(&line) {
             Some(_reply) => {
                 let _code = &_reply.code;
                 let _message: &str = &_reply.message;
                 stream.write(&_message.as_bytes())?;
                 stream.flush()?;
+
+                state = match &state {
+                    SmtpState::INIT => {
+                        match &_cmd {
+                            &"HELO" => SmtpState::READY,
+                            &"EHLO" => SmtpState::READY,
+                            &"RSET" => SmtpState::READY,
+                            _ => SmtpState::INIT,
+                        }
+                    }
+                    SmtpState::READY => {
+                        match &_cmd {
+                            &"MAIL" => SmtpState::MAIL,
+                            &"RSET" => SmtpState::READY,
+                            _ => SmtpState::READY,
+                        }
+                    }
+                    SmtpState::MAIL => {
+                        match &_cmd {
+                            &"RCPT" => SmtpState::RCPT,
+                            &"RSET" => SmtpState::READY,
+                            _ => SmtpState::MAIL,
+                        }
+                    }
+                    SmtpState::RCPT => {
+                        match &_cmd {
+                            &"DATA" => SmtpState::DATA,
+                            &"RSET" => SmtpState::READY,
+                            _ => SmtpState::RCPT,
+                        }
+                    }
+                    SmtpState::DATA => {
+                        match &_cmd {
+                            &"DATA" => SmtpState::READY,
+                            &"RSET" => SmtpState::READY,
+                            _ => SmtpState::DATA,
+                        }
+                    }
+                }
             }
             None => {
+                /* abort. */
                 stream.write(b"disconnect.")?;
                 stream.flush()?;
                 panic!();
@@ -44,13 +104,14 @@ pub fn handler(stream: &mut BufStream<TcpStream>) -> io::Result<()> {
     }
 }
 
-fn dispatch(command: String) -> Option<SmtpReply> {
-    let mut iter = command.split_whitespace();
-    let _method = iter.next();
-    
-    match _method {
-        Some("HELO") => {
-            let param1 = iter.next();
+fn dispatch(command: &String) -> Option<SmtpReply> {
+    let mut command_split = command.trim().split_whitespace();
+    let _cmd = command_split.next();
+
+    /* minimum implementation (see RFC5321 4.5.1) */
+    match _cmd {
+        Some("EHLO") => {
+            let param1 = command_split.next();
             println!("param1 {:?}", param1);
             let reply;
             match param1 {
@@ -61,7 +122,7 @@ fn dispatch(command: String) -> Option<SmtpReply> {
                     }
                 }
                 Some(param1) => {
-                    if iter.next() != None {
+                    if command_split.next() != None {
                         reply = SmtpReply{
                             code: 555,
                             message: format!(r"5.5.2 Syntax error. - smtpd-rs")
@@ -76,41 +137,52 @@ fn dispatch(command: String) -> Option<SmtpReply> {
             }
             Some(reply)
         }
-        Some("EHLO") => {
-            let peer_hostname = iter.next();
-            println!("{:?}", peer_hostname);
-            let reply = SmtpReply{
-                code: 250,
-                message: format!(r"ok at your service")
-            };
+        Some("HELO") => {
+            let param1 = command_split.next();
+            println!("param1 {:?}", param1);
+            let reply;
+            match param1 {
+                None => {
+                    reply = SmtpReply{
+                        code: 555,
+                        message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                    }
+                }
+                Some(param1) => {
+                    if command_split.next() != None {
+                        reply = SmtpReply{
+                            code: 555,
+                            message: format!(r"5.5.2 Syntax error. - smtpd-rs")
+                        };
+                    } else {
+                        reply = SmtpReply{
+                            code: 250,
+                            message: format!(r"ok at your service {:?}", param1)
+                        };
+                    }
+                }
+            }
             Some(reply)
         }
         Some("MAIL") => {
-            let from_address = iter.next();
+            let from_address = command_split.next();
             println!("{:?}", from_address);
-            let reply = SmtpReply{
-                code: 250,
-                message: format!(r"ok at your service")
-            };
-            Some(reply)
-        }
-        Some("RCPT") => {
-            let rcpt_address = iter.next();
-            println!("{:?}", rcpt_address);
-            let reply = SmtpReply{
-                code: 250,
-                message: format!(r"ok at your service")
-            };
-            Some(reply)
-        }
-        Some("DATA") => {
             let reply = SmtpReply{
                 code: 250,
                 message: format!(r"2.1.0 OK - smtpd-rs")
             };
             Some(reply)
         }
-        Some(".") => {
+        Some("RCPT") => {
+            let rcpt_address = command_split.next();
+            println!("{:?}", rcpt_address);
+            let reply = SmtpReply{
+                code: 250,
+                message: format!(r"2.1.0 OK - smtpd-rs")
+            };
+            Some(reply)
+        }
+        Some("DATA") => {
             let reply = SmtpReply{
                 code: 250,
                 message: format!(r"2.1.0 OK - smtpd-rs")
@@ -124,6 +196,13 @@ fn dispatch(command: String) -> Option<SmtpReply> {
             };
             Some(reply)
         }
+        Some("NOOP") => {
+            let reply = SmtpReply{
+                code: 250,
+                message: format!(r"2.1.0 OK - smtpd-rs")
+            };
+            Some(reply)
+        }
         Some("QUIT") => {
             let reply = SmtpReply{
                 code: 221,
@@ -131,8 +210,14 @@ fn dispatch(command: String) -> Option<SmtpReply> {
             };
             Some(reply)
         }
+        Some("VRFY") => {
+            let reply = SmtpReply{
+                code: 250,
+                message: format!(r"2.1.0 OK - smtpd-rs")
+            };
+            Some(reply)
+        }
         _ => {
-            println!("error");
             let reply = SmtpReply{
                 code: 555,
                 message: format!(r"5.5.2 Syntax error. - smtpd-rs")
@@ -156,12 +241,17 @@ fn test_handler() {
 
 #[test]
 fn test_dispatch() {
-    assert_eq!(dispatch("QUIT".parse().unwrap()),           Some(SmtpReply{code: 221, message: r"2.0.0 closing connection smtpd-rs".to_string()}));
-    assert_eq!(dispatch("HELO localhost".parse().unwrap()), Some(SmtpReply{code: 250, message: "ok at your service \"localhost\"".parse().unwrap()}));
-    assert_eq!(dispatch("EHLO localhost".parse().unwrap()), Some(SmtpReply{code: 250, message: r"ok at your service".parse().unwrap()}));
-    // assert_eq!(dispatch("MAIL FROM:postmaster@example.com".parse().unwrap()), Some(r"250 2.1.0 OK - smtpd-rs".parse().unwrap()));
-    // assert_eq!(dispatch("RCPT TO:postmaster@example.com".parse().unwrap()), Some(r"250 2.1.0 OK - smtpd-rs".parse().unwrap()));
-    assert_eq!(dispatch("DATA".parse().unwrap()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
-    assert_eq!(dispatch(".".parse().unwrap()),    Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
-    assert_eq!(dispatch("RSET".parse().unwrap()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"EHLO localhost".to_string()), Some(SmtpReply{code: 250, message: "ok at your service \"localhost\"".parse().unwrap()}));
+    assert_eq!(dispatch(&"HELO localhost".to_string()), Some(SmtpReply{code: 250, message: "ok at your service \"localhost\"".parse().unwrap()}));
+
+    assert_eq!(dispatch(&"MAIL FROM:postmaster@example.com".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"RCPT TO:postmaster@example.com".to_string()),   Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"DATA".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+
+    assert_eq!(dispatch(&"RSET".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"NOOP".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+    assert_eq!(dispatch(&"QUIT".to_string()), Some(SmtpReply{code: 221, message: r"2.0.0 closing connection smtpd-rs".to_string()}));
+    assert_eq!(dispatch(&"VRFY".to_string()), Some(SmtpReply{code: 250, message: r"2.1.0 OK - smtpd-rs".parse().unwrap()}));
+
+    assert_eq!(dispatch(&"blahblahblah".to_string()), Some(SmtpReply{code: 555, message: r"5.5.2 Syntax error. - smtpd-rs".to_string()}));
 }
