afd0b893f3cb69c27ee9a78c37270fa25a4a5545
Kei NIKADO <nikado.kei@gmail.com>
2018-09-27 15:10:22 +0900

    update: Mildir/new create file every 10 sec.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,10 +2,12 @@ extern crate hostname;
 extern crate bufstream;
 
 use bufstream::BufStream;
-use std::io;
+use std::io::{self, Write};
 use std::net::TcpListener;
 use std::thread::spawn;
 use std::{thread, time};
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::fs::OpenOptions;
 
 mod receiver;
 mod maildir;
@@ -23,6 +25,27 @@ fn smtpd(addr: &str) -> io::Result<()> {
         loop {
             maildir::scan().err();
             thread::sleep(time::Duration::from_millis(1000));
+        }
+    });
+
+    spawn(move || {
+        thread::sleep(time::Duration::from_millis(1000 * 3));
+        loop {
+            let start = SystemTime::now();
+            let since_the_epoch = start.duration_since(UNIX_EPOCH).unwrap();
+            let _filename = format!("tests/Maildir/new/mail.{:?}", since_the_epoch);
+            println!("filename {}", _filename);
+
+            {
+                let mut _file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .open(_filename).expect("file create");
+                _file.write_all(b"Hello, world!").unwrap();
+            }
+
+            thread::sleep(time::Duration::from_millis(1000 * 10));
         }
     });
     
